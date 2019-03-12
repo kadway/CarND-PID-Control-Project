84bfd522c42d0c5b0c948d0c4d901516689d71d6
João Gonçalves <miguel.joao.goncalves@gmail.com>
2019-03-12 19:43:20 +0100

    initialize the PID controller and add the option for passing on coefficients from argv

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,5 @@
 #include "PID.h"
-
+#include <iostream>
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
@@ -9,22 +9,31 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+    /**
+     * TODO: Initialize PID coefficients (and errors, if needed)
+     */
+    this->Kp = Kp_;
+    this->Ki = Ki_;
+    this->Kd = Kd_;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+    
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+    /**
+     * TODO: Update PID errors based on cte.
+     */
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+    
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    /**
+     * TODO: Calculate and return the total error
+     */
+    return Kp*p_error + Ki*i_error + Kd*d_error;  // TODO: Add your total error calc here!
 }
